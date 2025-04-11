8612c7d50c3d09c00ee924211841e5e29c939bb2
rohiit77 <rohiit77.77@gmail.com>
2025-04-11 01:06:39 -0700

    second profile added again

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const testimonials = [
         text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple."
     },
     {
+        name: "Basant",
+        photoUrl:  "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
+        text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple."
+    },
+    {
         name: "Rohit",
         photoUrl:  "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80",
         text: "This is simply unbelievable! I would be lost without Apple. The very best. Not able to tell you how happy I am with Apple."
